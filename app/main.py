a6ad6a0dd7e468b86cdfdc084d910708d485ceb8
ivan <ivanventik@gmail.com>
2025-11-09 18:38:19 +0200

    Soullution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.strip().split(" ")
+
+    if len(split_command) != 3 or split_command[0] != "cp":
+        return False
+
+    file_name = split_command[1]
+    new_file_name = split_command[2]
+
+    if file_name == new_file_name:
+        return False
+
+    try:
+        with open(file_name, "r") as file:
+            file_data = file.read()
+    except FileNotFoundError:
+        return False
+
+    try:
+        with open(new_file_name, "w") as new_file:
+            new_file.write(file_data)
+    except FileNotFoundError:
+        return False
