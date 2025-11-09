7a28204bc4d9a406b0266f2a6d78c841d2ae8b88
ivan <ivanventik@gmail.com>
2025-11-09 20:38:51 +0200

    Edited due to comments

--- app/main.py
+++ app/main.py
@@ -1,23 +1,18 @@
 def copy_file(command: str) -> None:
-    split_command = command.strip().split(" ")
+    split_command = command.strip().split()
 
     if len(split_command) != 3 or split_command[0] != "cp":
-        return False
+        return
 
     file_name = split_command[1]
     new_file_name = split_command[2]
 
     if file_name == new_file_name:
-        return False
+        return
 
     try:
-        with open(file_name, "r") as file:
-            file_data = file.read()
-    except FileNotFoundError:
-        return False
-
-    try:
-        with open(new_file_name, "w") as new_file:
-            new_file.write(file_data)
-    except FileNotFoundError:
-        return False
+        with (open(file_name, "r") as file,
+              open(new_file_name, "w") as new_file):
+            new_file.write(file.read())
+    except Exception:
+        return
